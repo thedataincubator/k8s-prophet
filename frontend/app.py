ed4f21736801eff8c7bcb1236450f0dff3b7b478
zachglassman <zach.glassman@gmail.com>
2018-03-13 16:59:43 -0700

    add index

--- frontend/app.py
+++ frontend/app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
-
 if __name__ == '__main__':
   app.run(port=33507)
